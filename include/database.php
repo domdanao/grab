735d8b9507235cc1d3b94c6a3e171fe8856fa2d3
Dom Danao <dom@danao.co>
2013-09-02 12:04:55 +0800

    commit 09022013_1204

--- include/database.php
+++ include/database.php
@@ -2,15 +2,15 @@
 // GRAB Database Login
 
 if (isset($_SERVER["RDS_HOSTNAME"])) {
-	
+	$dbname = 'grab';
 	$dblink = mysql_connect(
 		$_SERVER["RDS_HOSTNAME"] . ":" . $_SERVER["RDS_PORT"],
 		$_SERVER["RDS_USERNAME"],
 		$_SERVER["RDS_PASSWORD"])
 		or
 		die ("ERROR:" . mysql_error());
-	mysql_select_db($_SERVER["RDS_DB_NAME"]);
-	print "CONNECTED REMOTE: " . $_SERVER["RDS_DB_NAME"];
+	mysql_select_db($dbname);
+	print "CONNECTED REMOTE: $dbname <>" . $_SERVER["RDS_DB_NAME"];
 
 } else {
 
