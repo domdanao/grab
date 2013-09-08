a6f8a31a4a115b7b58163c3b805d94bf3d95908c
Dom Danao <dom@danao.co>
2013-09-08 16:49:10 +0800

    grab_unli 09-08-2013 1649

--- globe/grab_unli.php
+++ globe/grab_unli.php
@@ -91,7 +91,7 @@ if ( empty( $_REQUEST['others'] ) ) {
 		// Do sanity check first
 		if ( $no_buy_item = sanity_check( $sender, $grabs ) ) {
 			// Do not allow buying for this grab item
-			list( $the_no_buy_item_gid, $the_no_buy_item_keyword, $the_buy_item_grab_end ) = explode( ":", $no_buy_item[0] );
+			list( $the_no_buy_item_gid, $the_no_buy_item_keyword, $the_buy_item_grab_end ) = explode( "/", $no_buy_item[0] );
 			$formatted_grab_end = date( "M j, Y g:i:s A", strtotime( $the_buy_item_grab_end ) );
 			$msg = "GRAB: Covered ka na hanggang matapos grabs for " . strtoupper( $the_no_buy_item_keyword ) . " until " . $formatted_grab_end . " so no need to buy more unli-grabs. " . $no_buy_item[0];
 			// Not the most elegant, but will work
@@ -312,7 +312,7 @@ function sanity_check( $sender, $grabs ) {
 		$grab_end_timestamp = strtotime($row['grab_end']);
 		$unlisub_end_timestamp = strtotime($unlisub['end_time']);
 		if ( $unlisub_end_timestamp > $grab_end_timestamp ) {
-			$no_more_buys[] = $gid.':'.$item.':'.$grab_end;
+			$no_more_buys[] = $gid.'/'.$item.'/'.$grab_end;
 		}
 	}
 	return $no_more_buys;
