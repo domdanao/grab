cafa9a24c4a4f8a86aaa17036771a85a665f9a5c
Dom Danao <dom@danao.co>
2013-09-27 02:04:52 -0700

    removed function from results

--- globe/utilities/results.php
+++ globe/utilities/results.php
@@ -1,6 +1,6 @@
 <?php
 
-include '../include/config.php';
+require '../include/config.php';
 
 print "OK...";
 
@@ -12,30 +12,4 @@ $row = mysql_fetch_assoc( $r );
 
 print_r( $row );
 
-
-##################################################
-// Get total holdtime
-function total_hold_time( $msisdn, $table, $timenow = 0 ) {
-	global $dblink;
-
-	$totalholdtime = 0;
-
-	$query = "SELECT SUM( lost_time-grab_time ) AS totalholdtime FROM `$table` WHERE `msisdn` = '$msisdn' AND lost_time <> 0";
-	$result = mysql_query( $query );
-	if ( $result !== FALSE ) {
-		$row = mysql_fetch_assoc( $result );
-		$holder_time = is_holder( $msisdn, $table );
-
-		if ( $holder_time ) {
-			$time_so_far = $row['totalholdtime'];
-			$inc_time = $timenow-$holder_time;
-			// incremental time added to totalholdtime
-			$totalholdtime = $time_so_far+$inc_time;
-		} else {
-			$totalholdtime = $row['totalholdtime'];
-		}
-	}
-
-	return $totalholdtime;
-}
 ?>
