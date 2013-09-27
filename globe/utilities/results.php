2ce4f9c986181b58a92550892147ea337ebbf6e0
Dom Danao <dom@danao.co>
2013-09-27 02:40:57 -0700

    more or less final results.php scritp

--- globe/utilities/results.php
+++ globe/utilities/results.php
@@ -2,14 +2,19 @@
 
 require '../include/config.php';
 
-print "OK...";
-
 $grab_table = $_REQUEST['table'];
 
+$results = array();
 $q = "SELECT DISTINCT( msisdn ) AS phone FROM `" . $grab_table . "`";
 $r = mysql_query( $q );
-while ($row = mysql_fetch_array( $r )) {
-	echo $row['phone'] . "\n";
+while ($row = mysql_fetch_assoc( $r )) {
+	$phone = $row['phone'];
+	$total_time = total_hold_time($phone, $grab_table);
+	$results[$phone] = $total_time;
 }
 
+asort($results);
+foreach ($results as $key => $val) {
+	print "$key\t$val\n";
+}
 ?>
