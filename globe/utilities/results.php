0bcd1086a215bd2a11661ba17410664cf1cd8c82
Dom Danao <dom@danao.co>
2013-09-27 22:57:00 -0700

    results

--- globe/utilities/results.php
+++ globe/utilities/results.php
@@ -9,12 +9,45 @@ $q = "SELECT DISTINCT( msisdn ) AS phone FROM `" . $grab_table . "`";
 $r = mysql_query( $q );
 while ($row = mysql_fetch_assoc( $r )) {
 	$phone = $row['phone'];
-	$total_time = total_hold_time($phone, $grab_table);
+	$total_time = results_hold_times($phone, $grab_table);
 	$results[$phone] = $total_time;
 }
 
 arsort($results);
 foreach ($results as $key => $val) {
 	print "$key\t$val\n";
+}
+
+##################################################
+// Get total holdtime
+function results_hold_times( $msisdn, $table, $timenow = 0 ) {
+	global $dblink;
+
+	$totalholdtime = 0;
+
+	$parts = preg_split('/_/', $table);
+	$gid = $parts[2];
+	$q = "SELECT UNIX_TIMESTAMP(grab_end) AS end_time FROM `$table` WHERE `gid` = $gid";
+	$rs = mysql_query($q);
+	$rw = mysql_fetch_assoc($rs);
+	$time_end = $rw['end_time'];
+	
+	$query = "SELECT SUM( lost_time-grab_time ) AS totalholdtime FROM `$table` WHERE `msisdn` = '$msisdn'";
+	$result = mysql_query( $query );
+	if ( $result !== FALSE ) {
+		$row = mysql_fetch_assoc( $result );
+		$holder_time = is_holder( $msisdn, $table );
+
+		if ( $holder_time ) {
+			$time_so_far = $row['totalholdtime'];
+			$inc_time = $time_end-$holder_time;
+			// incremental time added to totalholdtime
+			$totalholdtime = $time_so_far+$inc_time;
+		} else {
+			$totalholdtime = $row['totalholdtime'];
+		}
+	}
+
+	return $totalholdtime;
 }
 ?>
