770638d784a55b43237e1234e0f5c94dc3a0196b
Dom Danao <dom@danao.co>
2013-09-29 01:00:38 -0700

    results revision

--- globe/utilities/results.php
+++ globe/utilities/results.php
@@ -3,8 +3,8 @@
 require '../include/config.php';
 
 // output headers so that the file is downloaded rather than displayed
-header('Content-Type: text/csv; charset=utf-8');
-header('Content-Disposition: attachment; filename=data.csv');
+//header('Content-Type: text/csv; charset=utf-8');
+//header('Content-Disposition: attachment; filename=data.csv');
 
 // create a file pointer connected to the output stream
 $output = fopen('php://output', 'w');
